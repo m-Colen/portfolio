e633988710752928c60761ea77f853a21be64c75
m-colen <mzcolen@gmail.com>
2022-11-27 00:23:33 -0600

    Fixed bug with contact from
    Had a bug with contact form not working due to React.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,6 +9,7 @@ export default function Contact() {
       <h2>Contact</h2>
       <div>
         <form name="contact" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
           <p>
             <label>
               Your Name: <input type="text" name="name" />
